89017cf398528890f6df3e7ed2862212dbb065bd
Alicia Toshima <toshimaalicia@gmail.com>
2019-03-06 20:08:54 -0800

    it worked

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
 // pigLatin function
+
 var pigLatin = function(string) {
   if (string[0] == "a" //vowels
       || string[0] === "e"
@@ -6,7 +7,7 @@ var pigLatin = function(string) {
      || string[0] === "o"
      || string[0] === "u"
      ) {
-    var done = string[1,string.length +1] + string.charAt(0) +"way"
+    var done = string.substring(1,string.length +1) + string.charAt(0) +"way"
   } else if (string.length >0 && string.substring(0,2) === "qu") { //qu position 0 case
     var done = string.substring(2,string.length +1) + string.substring(0,2) + "ay"
   } else if (string.length >0 && string.substring(1,3) === "qu") { // qu position 1 case
@@ -17,21 +18,31 @@ var pigLatin = function(string) {
   return done
 };
 
-pigLatin("hello")
+
+var mapping = function(sentence) {
+  var array = sentence.split(" ")
+  var array2 = []
+  array.forEach(function(word) {
+    var mapResult = array2.push(pigLatin(word));
+  })
+  return array2.join(" ")
+}
+
+
+//pigLatin("pig latin")
+
+
+
 
 $(document).ready(function() {
   $("form#leap-year").submit(function(event) {
     event.preventDefault();
-    var year = parseInt($("input#year").val());
-    var result =pigLatin(year);
+    var sentence = $("input#sentence").val();
+    var result = mapping(sentence);
 
-    $(".year").text(year);
+    $(".year").text(result);
 
-    if (!result) {                 // same as writing if (result === false)
-      $(".not").text("not");
-    } else {
-      $(".not").text("");
-    }
+
 
     $("#result").show();
   });
